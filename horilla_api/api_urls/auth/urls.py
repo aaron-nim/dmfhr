05b8f257a2183c3e26374eb4e7aa5897191f2ae1
David Olarte <david.olarte@digitalmediafactory.ch>
2026-08-11 11:23:04 +0800

    Add iframe authentication endpoint

--- horilla_api/api_urls/auth/urls.py
+++ horilla_api/api_urls/auth/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
-from ...api_views.auth.views import LoginAPIView
+from ...api_views.auth.views import (
+    LoginAPIView,
+    IframeTokenAPIView,
+)
 
-urlpatterns = [path("login/", LoginAPIView.as_view())]
+urlpatterns = [
+    path("login/", LoginAPIView.as_view()),
+    path("iframe-token/", IframeTokenAPIView.as_view()),
+]
